f4cd2379a6c47502cea88ec65555c184505633b0
Ajij <mohammadajij120386@gmail.com>
2024-06-07 14:09:38 +0600

    Ajij

---
+++ Personal/Gmail Cover Letter.c
@@ -0,0 +1,14 @@
+Dear Sir,
+
+
+I am excited to submit my application for the IT Intern role  at BJIT Career, as advertised on Linkedin. With my qualifications outlined in the attached resume,
+ I am confident in my ability to technical skill and soft skill and I contribute significantly to your team's success.
+
+ This opportunity aligns perfectly with my professional goals and i am eager to discuss how i can leverage my expertise to drive results and be a
+ a valuable asset to BJIT Career.
+
+Thank you for considering my application, and I look forward to the opportunity to further explore my candidacy;
+
+Best regards,
+Mohammad Ajij
+Phone: 01609016477
